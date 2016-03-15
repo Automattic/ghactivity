60b8870f3ebbd9da59e337449d5a39b85e9a9d4e
Jeremy Herve <jeremy@tagada.hu>
2016-03-15 08:41:48 +0100

    Basic structure for our plugin

---
+++ ghactivity.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Plugin Name: GHActivity
+ * Plugin URI: http://jeremy.hu
+ * Description: Build reports of all your GitHub activity.
+ * Version: 1.0
+ * Author: Jeremy Herve
+ * Author URI: http://jeremy.hu
+ * Text Domain: ghactivity
+ * Domain Path: /languages/
+ * License: GPL2
+ */
+
+ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+define( 'GHACTIVITY__VERSION',     '1.0' );
+define( 'GHACTIVITY__PLUGIN_DIR',  plugin_dir_path( __FILE__ ) );
+
+class Jeherve_GHActivity {
+	private static $instance;
+
+	static function get_instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new Jeherve_GHActivity;
+		}
+		return self::$instance;
+	}
+
+	private function __construct() {
+		// Load translations
+		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+		// Load plugin
+		add_action( 'plugins_loaded', array( $this, 'load_plugin' ) );
+	}
+
+	public function load_textdomain() {
+		load_plugin_textdomain( 'ghactivity', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	public function load_plugin() {
+		// Load core functions.
+		require_once( GHACTIVITY__PLUGIN_DIR . 'core.ghactivity.php' );
+
+		// Settings panel.
+		if ( is_admin() ) {
+			require_once( GHACTIVITY__PLUGIN_DIR . 'admin.ghactivity.php' );
+		}
+	}
+}
+// And boom.
+Jeherve_GHActivity::get_instance();
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== GHActivity ===
+Contributors: jeherve
+Tags: GitHub, Tracking, Productivity,
+Stable tag: 1.0
+Requires at least: 4.4.2
+Tested up to: 4.4.2
+
+Monitor all your activity on GitHub, and posts reports about it on your WordPress site.
+
+== Description ==
+
+Monitor all your activity on GitHub, and posts reports about it on your WordPress site.
+
+If you find bugs, you can report them [here](http://wordpress.org/support/plugin/ghactivity), or submit a pull request [on GitHub](https://github.com/jeherve/ghactivity/).
+
+== Installation ==
+
+1. Install the GHActivity plugin via the WordPress.org plugin repository, or via your dashboard
+2. Activate the plugin
+3. Go to Settings > GHActivity Manager in your dashboard and configure the plugin.
+4. Enjoy! :)
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
