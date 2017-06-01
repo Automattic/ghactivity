fb8b5c277bcdf5dadce376a99e7c2a4f78605168
Jeremy Herve <jeremy@tagada.hu>
2017-06-01 14:42:11 +0200

    Avoid empty post content when no action is defined in GH API.

--- core.ghactivity.php
+++ core.ghactivity.php
@@ -161,11 +161,10 @@ class GHActivity_Calls {
 	 *
 	 * @return string $link_html HTML link matching the action recorded by GitHub.
 	 */
-	private function get_event_link( $event, $action ) {
+	private function get_event_link( $event, $action = '' ) {
 		if (
 			empty( $event )
 			|| empty( $event->type )
-			|| empty( $action )
 		) {
 			return '';
 		}
