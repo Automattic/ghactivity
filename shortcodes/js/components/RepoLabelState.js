c6f974683ad6478d1babeabda98101cfc9ad07b4
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2018-11-12 13:54:09 -0600

    Update RepoLabelState ouput to be more useful

--- shortcodes/js/components/RepoLabelState.js
+++ shortcodes/js/components/RepoLabelState.js
@@ -6,7 +6,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Doughnut } from 'react-chartjs-2';
-// import 'chartjs-plugin-labels'
+import 'chartjs-plugin-labels'
 
 class RepoLabelState extends Component {
 	constructor() {
@@ -46,23 +46,19 @@ class RepoLabelState extends Component {
 
 	// data.current_label_state[0].Pri
 	renderDoughnut(dataObject) {
-		const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
-		'#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		'#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
-		'#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-		'#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
-		'#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		'#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
-		'#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		'#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-		'#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
-		const elementCount = Object.keys( dataObject ).length;
-		const colors = colorArray.slice( 0, elementCount )
+		const dataEntries = Object.entries( dataObject );
+		dataEntries.sort( (a, b) => a[1] - b[1] );
+
+		const labels = dataEntries.map( e => e[0] );
+		const data = dataEntries.map( e => e[1] );
+		const elementCount = labels.length;
+
+		const colors = this.getRandomColors( elementCount )
 		const chartArgs = {
-			labels: Object.keys( dataObject ),
+			labels,
 			datasets: [
 				{
-					data: Object.values( dataObject ),
+					data,
 					backgroundColor: colors,
 					hoverBackgroundColor: colors,
 				},
@@ -74,25 +70,45 @@ class RepoLabelState extends Component {
 				display: false,
 			},
 			plugins: {
-				labels: {
-					// render: 'label',
-					render: 'value',
-					fontColor: '#000',
-					// position: 'outside',
-					// textMargin: 15,
-					// outsidePadding: 10,
-					// overlap: false,
-					// showZero: true,
-				}
+				labels: [
+					{
+						render: args => args.percentage < 2 ? '' : args.label,
+						fontColor: '#000',
+						position: 'outside',
+						textMargin: 20,
+						outsidePadding: 20,
+					},
+					{
+						render: args => args.percentage < 2 ? '' : args.value,
+						fontColor: '#000',
+					}
+				]
 			}
 		}
 
 		return <Doughnut data={ chartArgs } options={ options } />
 	}
 
-	renderSection( sectionType ) {
+	// renderSection( sectionType ) {
+	// 	const { currentLabelState, previousLabelState } = this.state;
+	// }
 
+	// https://stackoverflow.com/a/1484514/3078381
+	getRandomColors( count ) {
+		const colors = [];
+		const letters = '0123456789ABCDEF';
+		for ( let c = 0; c < count; c++ ) {
+			let color = '#';
+			for ( let i = 0; i < 6; i++ ) {
+				color += letters[Math.floor(Math.random() * 16)];
+			}
+			colors.push(color);
+		}
+		return colors;
 	}
+
+
+
 
 	render() {
 		const { currentLabelState, previousLabelState } = this.state;
@@ -105,28 +121,55 @@ class RepoLabelState extends Component {
 			)
 		}
 
+		const priLabeledCount = Object.values( currentLabelState[0].Pri ).reduce( ( acc, val ) => acc + val, 0 );
+		const priLabeledPercentage = Math.floor( priLabeledCount / currentLabelState[1] * 100 );
+
+		// const noneMostUsedLabels =
+
+		let topNoneLabels = Object.entries( currentLabelState[0].none );
+		topNoneLabels.sort( (a, b) => b[1] - a[1] );
+		const tenPercentCount = Math.floor( topNoneLabels.length / 10 );
+		topNoneLabels = topNoneLabels.slice(0, tenPercentCount)
 
 		return (
 			<div>
+				<hr />
+				{/* Type Section */}
 				<div>
-					{ JSON.stringify( currentLabelState ) }
+					<p>
+						We have { currentLabelState[1] } open issues, including:
+					</p>
+					<ul>
+						<li>{ currentLabelState[0].Type['[Type] Bug'] } open bugs</li>
+						<li>{ currentLabelState[0].Type['[Type] Enhancement'] } open enhancement requests</li>
+					</ul>
+					<div>
+						{ this.renderDoughnut( currentLabelState[0].Type ) }
+					</div>
 				</div>
+				<hr />
+				{/* Priority Section */}
 				<div>
-					{ JSON.stringify( previousLabelState ) }
+					<p>
+						{ priLabeledPercentage }% of the reported issues have its priority set: { priLabeledCount } of { currentLabelState[1] }. These are broken down as:
+					</p>
+					<div>
+						{ this.renderDoughnut( currentLabelState[0].Pri ) }
+					</div>
 				</div>
-
-				{/* <div>
-					{ this.renderDoughnut( currentLabelState[0].Pri ) }
-				</div>
+				<hr />
+				{/* none Section */}
 				<div>
-					{ this.renderDoughnut( currentLabelState[0].Type ) }
+					<p>
+					The areas with the most issues:
+					</p>
+					<ul>
+						{ topNoneLabels.map( (labelArray, idx) => <li key={idx} >{labelArray[0]} ({labelArray[1]} open issues)</li>) }
+					</ul>
+					<div>
+						{ this.renderDoughnut( currentLabelState[0].none ) }
+					</div>
 				</div>
-				<div>
-					{ this.renderDoughnut( currentLabelState[0].Status ) }
-				</div>
-				<div>
-					{ this.renderDoughnut( currentLabelState[0].none ) }
-				</div> */}
 			</div>
 
 		);
